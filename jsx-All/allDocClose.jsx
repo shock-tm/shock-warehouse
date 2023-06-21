119eadb7b51bb3a2c1e7462573486899032f019b
shock tm <catdtp@gmail.com>
2023-06-21 13:17:18 +0900

    2023/06/21

--- jsx-All/allDocClose.jsx
+++ jsx-All/allDocClose.jsx
@@ -4,7 +4,8 @@ allDocClose.jsx
 (c)2020 Shock tm
 全てのドキュメントを閉じるスクリプト
 変更があったドキュメントが存在する場合は問い合わせるダイアログ表示
-2020-01-14 ver1.0
+2023-06-21 ver1.0.1 ドキュメントの保持方法を変更
+2020-01-14 ver1.0.0
 */
 var idReg=new RegExp("indesign","i");
 if(idReg.test(app.name)){
@@ -16,6 +17,7 @@ function allDocClose(ID){
     if(app.documents.length!=0){
         var bool=false;
         var docNames="";
+        var docList=[];
         for(var d=0;d<app.documents.length;d++){
             if(ID){
                 if(app.documents[d].modified){
@@ -28,13 +30,14 @@ function allDocClose(ID){
                     docNames+="・ "+app.documents[d].name+"\n";
                 };
             };
+            docList.push(app.documents[d]);
         };
         var cf=false;
         if(bool){
             cf=confirm ("閉じる前に保存しますか？\n\n※保存対象ドキュメント：\n"+docNames);
         };
         if(cf){
-            while(app.documents.length!=0){
+            for(docs in docList){
                 if(ID){
                     app.documents[0].close(SaveOptions.yes);
                 }else{
@@ -42,7 +45,7 @@ function allDocClose(ID){
                 };
             };
         }else{
-            while(app.documents.length!=0){
+            for(docs in docList){
                 if(ID){
                     app.documents[0].close(SaveOptions.no);
                 }else{
